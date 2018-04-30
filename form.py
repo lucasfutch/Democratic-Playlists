6e32964a83c97a51b701178f2a05fe012de4dd69
lucasfutch <lucasfutch@gmail.com>
2018-04-30 10:55:16 +0400

    added bootstrap html on form.py

--- form.py
+++ form.py
@@ -5,10 +5,10 @@ import cgi
 import cgitb
 
 def htmlHead():
-	print("<!DOCTYPE HTML><html><head><title>Playing Song</title><link rel='stylesheet' type='text/css' href='../offcanvas.css'></head><body>")	
+	print("<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1'><meta name='description' content=''><meta name='author' content=''><link rel='icon' href='favicon.ico'><title>Democratic Playlists</title><link href='../DemocraticPlaylists/css/bootstrap.min.css' rel='stylesheet'><link href='../DemocraticPlaylists/css/ie10-viewport-bug-workaround.css' rel='stylesheet'><link href='../DemocraticPlaylists/cover.css' rel='stylesheet'><script src='../DemocraticPlaylists/js/ie-emulation-modes-warning.js'></script><script src='https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js'></script><script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script><link href='https://fonts.googleapis.com/css?family=Anton|Indie+Flower|Jua' rel='stylesheet'></head><body><div class='site-wrapper'><div class='site-wrapper-inner'><div class='cover-container'><div class='masthead clearfix'><div class='inner'><h3 class='masthead-brand'>Democratic Playlists</h3><nav><ul class='nav masthead-nav'><li><a href='../DemocraticPlaylists/index.html'>Home</a></li><li class='active'><a href='#'>Currently Playing...</a></li><li><a href='../DemocraticPlaylists/about.html'>About</a></li></ul></nav></div></div><div class='inner cover'>")
 
 def htmlTail():
-	print("</body></html>")
+	print("<div class='mastfoot'><div class='inner'><p>Lucas Futch | Kevin Long Noll | <a href = 'https://www.bigbustours.com/en/new-york/top-10-things-to-do-in-new-york/'>Image Source</a> </p></div></div></div></div></div><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script><script>window.jQuery || document.write('<script src='../DemocraticPlaylists/js/vendor/jquery.min.js'><\/script>')</script><script src='../DemocraticPlaylists/js/bootstrap.min.js'></script><script src='../DemocraticPlaylists/js/ie10-viewport-bug-workaround.js'></script></body></html>")
 
 # save the computed results
 def saveResults(name):
@@ -56,9 +56,9 @@ def main():
 
 	song1, song2, song3 = readResults(results)
 
-	print("<h1>Song 1 Votes:", song1, "</h1>")
-	print("<h1>Song 2 Votes:", song2, "</h1>")
-	print("<h1>Song 3 Votes:", song3, "</h1>")
+	print("<h2>Song 1 Votes:", song1, "</h2>")
+	print("<h2>Song 2 Votes:", song2, "</h2>")
+	print("<h2>Song 3 Votes:", song3, "</h2>")
 
 
 htmlHead()
