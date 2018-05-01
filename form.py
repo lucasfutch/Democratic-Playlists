055e9f7fbe8fef5775ae4fdb5ebbcd975e7ef796
lucasfutch <lucasfutch@gmail.com>
2018-05-01 12:51:19 +0400

    added song names and changed font. added link to google website

--- form.py
+++ form.py
@@ -5,10 +5,91 @@ import cgi
 import cgitb
 
 def htmlHead():
-	print("<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1'><meta name='description' content=''><meta name='author' content=''><link rel='icon' href='favicon.ico'><title>Democratic Playlists</title><link href='../DemocraticPlaylists/css/bootstrap.min.css' rel='stylesheet'><link href='../DemocraticPlaylists/css/ie10-viewport-bug-workaround.css' rel='stylesheet'><link href='../DemocraticPlaylists/cover.css' rel='stylesheet'><script src='../DemocraticPlaylists/js/ie-emulation-modes-warning.js'></script><script src='https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js'></script><script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script><link href='https://fonts.googleapis.com/css?family=Anton|Indie+Flower|Jua' rel='stylesheet'></head><body><div class='site-wrapper'><div class='site-wrapper-inner'><div class='cover-container'><div class='masthead clearfix'><div class='inner'><h3 class='masthead-brand'>Democratic Playlists</h3><nav><ul class='nav masthead-nav'><li><a href='../DemocraticPlaylists/index.html'>Home</a></li><li class='active'><a href='#'>Currently Playing...</a></li><li><a href='../DemocraticPlaylists/about.html'>About</a></li></ul></nav></div></div><div class='inner cover'>")
+	print(""" 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Democratic Playlists</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../DemocraticPlaylists/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../DemocraticPlaylists/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../DemocraticPlaylists/cover.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../DemocraticPlaylists/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="https://fonts.googleapis.com/css?family=Anton|Indie+Flower|Jua" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Democratic Playlists</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li><a href="../DemocraticPlaylists/index.html">Home</a></li>
+                  <li class="active"><a href="#">Currently Playing...</a></li>
+                  <li><a href="../DemocraticPlaylists/about.html">About</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+""")
+
 
 def htmlTail():
-	print("<div class='mastfoot'><div class='inner'><p>Lucas Futch | Kevin Long Noll | <a href = 'https://www.bigbustours.com/en/new-york/top-10-things-to-do-in-new-york/'>Image Source</a> </p></div></div></div></div></div><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script><script>window.jQuery || document.write('<script src='../DemocraticPlaylists/js/vendor/jquery.min.js'><\/script>')</script><script src='../DemocraticPlaylists/js/bootstrap.min.js'></script><script src='../DemocraticPlaylists/js/ie10-viewport-bug-workaround.js'></script></body></html>")
+	print(""" 
+		  <div class="mastfoot">
+            <div class="inner">
+              <p>Lucas Futch | Kevin Long Noll | <a class = "myLink" href = "https://www.bigbustours.com/en/new-york/top-10-things-to-do-in-new-york/">Image Source</a> </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../DemocraticPlaylists/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../DemocraticPlaylists/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+""")
 
 # save the computed results
 def saveResults(name):
@@ -44,11 +125,13 @@ def main():
 	song2 = 0
 	song3 = 0
 
+	song1Name = "God's Plan - Drake"
+
 	
 	form = cgi.FieldStorage()
 
 	if "song" not in form:
-		print("<h1>Please select a song!</h1>")
+		print("<h1 style = 'margin-bottom:40px;'>Please select a song!</h1>")
 	else:
 		songSelection = form['song'].value
 		saveResults(songSelection)
@@ -56,9 +139,9 @@ def main():
 
 	song1, song2, song3 = readResults(results)
 
-	print("<h2>Song 1 Votes:", song1, "</h2>")
-	print("<h2>Song 2 Votes:", song2, "</h2>")
-	print("<h2>Song 3 Votes:", song3, "</h2>")
+	print("<h2>", song1Name, ":", song1, " votes</h2>")
+	print("<h2>Dua Lipa &mdash; IDGAF:", song2, " votes</h2>")
+	print("<h2>Ariana Grande &mdash; No Tears Left to Cry:", song3, " votes</h2>")
 
 
 htmlHead()
